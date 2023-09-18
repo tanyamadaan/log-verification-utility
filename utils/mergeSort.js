b2f026686dab0c55f30974fceeba801a09874531
Tanya Madaan <tanyamadaan@Tanyas-MacBook-Air.local>
2023-09-18 16:04:54 +0530

    Removed unused files

--- utils/mergeSort.js
+++ utils/mergeSort.js
@@ -92,11 +92,4 @@ const sortMerge = (domain, directory, destination) => {
     console.trace(err);
   }
 };
-
 module.exports = { sortMerge };
-// const cwd = __dirname;
-// const destination = path.join(cwd, "./testCopy.json");
-// const directory = path.join(cwd, "../public/logs/flow4");
-// //const directory = '/Users/tanyamadaan/Desktop/Log-Verification/reference-implementations/utilities/log-validation-utility/utils/logs/Flow1'
-
-// sortMerge(directory, destination);

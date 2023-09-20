317a0cbd6a38c53048ab1d475fa23556535bce02
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2023-09-20 13:25:08 +0530

    Starting server development.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,23 +3,27 @@ const router = express.Router();
 const service = require("../../services/service");
 const { validateLog } = require("../../services/cbCheck.service");
 
-router.post("/validateSchema/:path", (req, res) => {
-  const path = req.params.path;
-  const data = req.body;
-  const result = service.schemaValidation(domain, data, path);
-  res.json(result);
-});
+router.get("/", (req, res) => {
+  res.json("Hello There!")
+})
 
-router.post("/CheckContext/:path", (req, res) => {
-  const path = req.params.path;
-  const data = req.body.context;
-  const result = service.checkContext(data, path);
-  res.json(result);
-});
+// router.post("/validateSchema/:path", (req, res) => {
+//   const path = req.params.path;
+//   const data = req.body;
+//   const result = service.schemaValidation(domain, data, path);
+//   res.json(result);
+// });
 
-router.post("/ValidateLog/:domain", (req, res) => {
-  const domain = req.params.domain;
-  validateLog(domain);
-});
+// router.post("/CheckContext/:path", (req, res) => {
+//   const path = req.params.path;
+//   const data = req.body.context;
+//   const result = service.checkContext(data, path);
+//   res.json(result);
+// });
+
+// router.post("/ValidateLog/:domain", (req, res) => {
+//   const domain = req.params.domain;
+//   validateLog(domain);
+// });
 
 module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = require("./routes/routes")
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(router)
+
+app.listen(port, () => {
+  console.log(`Log Validation running on PORT ${port}`)
+})
